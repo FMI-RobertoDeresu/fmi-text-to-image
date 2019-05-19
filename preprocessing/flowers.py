7e255cdc398ce449ad1f08043eb5aacc64e47c8a
Roberto Deresu <roberto.deresu@gmail.com>
2019-05-19 22:28:12 +0300

    -

---
+++ preprocessing/flowers.py
@@ -0,0 +1,55 @@
+import os
+import json
+import pathlib
+from PIL import Image
+
+resize = False
+flowers_raw_dataset_path = pathlib.Path("../tmp/datasets/flowers")
+flowers_processed_dataset_path = pathlib.Path("../datasets/flowers")
+
+
+def make_flowers_dataset():
+    labels_file_path = os.path.join(flowers_raw_dataset_path, "labels.txt")
+    with open(labels_file_path, 'r') as f:
+        labels = [int(x) for x in f.readlines()]
+
+    captions = dict()
+    text_class_dirs = list(os.walk(os.path.join(flowers_raw_dataset_path, "text")))[1:]
+    for root, _, file_names in text_class_dirs:
+        for file_name in file_names:
+            file_path = os.path.join(root, file_name)
+            image_name = file_name.split(".")[0]
+            with open(file_path, 'r') as f:
+                captions[image_name] = [x.rstrip("\n\r") for x in f.readlines()]
+
+    meta = []
+
+    images_dirs_path = os.path.join(flowers_raw_dataset_path, "jpg")
+    image_files = list(os.walk(images_dirs_path))[0][2]
+    for index, image_file_name in enumerate(image_files):
+        image_name = image_file_name.split(".")[0]
+        label_folder_name = str(labels[index]).rjust(4, "0")
+
+        old_file_path = os.path.join(images_dirs_path, image_file_name)
+        new_file_path = os.path.join(flowers_processed_dataset_path, label_folder_name, image_file_name)
+        img = Image.open(old_file_path)
+        if resize:
+            img = img.resize((128, 128), Image.ANTIALIAS)
+
+        pathlib.Path(new_file_path).parent.mkdir(parents=True, exist_ok=True)
+        img.save(new_file_path)
+
+        meta.append({
+            "image": os.path.join(label_folder_name, image_file_name),
+            "label": labels[index],
+            "captions": captions[image_name]
+        })
+
+    meta_file_path = os.path.join(flowers_processed_dataset_path, "meta.json")
+    pathlib.Path(meta_file_path).parent.mkdir(parents=True, exist_ok=True)
+    with open(meta_file_path, 'w+') as f:
+        json.dump(meta, f, indent=4)
+
+
+if __name__ == "__main__":
+    make_flowers_dataset()
---
+++ preprocessing/mnist.py
@@ -0,0 +1,58 @@
+import tensorflow as tf
+import pathlib
+import numpy as np
+import os
+import time
+import json
+from PIL import Image
+
+dataset_path = pathlib.Path("../datasets/mnist/{}".format(int(time.time())))
+dataset_path.mkdir(parents=True, exist_ok=True)
+
+letter_name = {
+    "0": "zero",
+    "1": "one",
+    "2": "two",
+    "3": "three",
+    "4": "four",
+    "5": "five",
+    "6": "six",
+    "7": "seven",
+    "8": "eight",
+    "9": "nine",
+}
+
+(x_train, y_train), _ = tf.keras.datasets.mnist.load_data()
+rnd_index_list = np.random.randint(60000, size=(1000, 3))
+img_pad_with = 9 * (3 - 1)
+
+meta = []
+for index_list in rnd_index_list:
+    images = [np.array(x_train[x], dtype='float') for x in index_list]
+    labels = [str(y_train[x]) for x in index_list]
+
+    labels_names = [letter_name[x] for x in labels]
+    label_name = " ".join(labels_names)
+
+    image = np.concatenate(tuple(images), axis=1)
+    image = np.pad(image, ((img_pad_with, img_pad_with), (0, 0)), mode="constant", constant_values=0)
+
+    img = Image.fromarray(image)
+    img = img.resize((128, 128), Image.ANTIALIAS)
+    img = img.convert("1")
+
+    image_file_name = "img_{}.png".format("_".join(labels))
+    image_file_path = os.path.join(dataset_path, "imgs", image_file_name)
+    pathlib.Path(image_file_path).parent.mkdir(parents=True, exist_ok=True)
+    img.save(image_file_path)
+
+    meta.append({
+        "image": os.path.join("imgs", image_file_name),
+        "label": labels,
+        "captions": label_name
+    })
+
+meta_file_path = os.path.join(dataset_path, "meta.json")
+pathlib.Path(meta_file_path).parent.mkdir(parents=True, exist_ok=True)
+with open(meta_file_path, 'w+') as f:
+    json.dump(meta, f, indent=4)
