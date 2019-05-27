1225ab2f10a541fdc1b49c97f1ae541cdafd6f46
Roberto Deresu <roberto.deresu@gmail.com>
2019-05-27 23:12:28 +0300

    -

---
+++ models/word2vec.py
@@ -0,0 +1,61 @@
+import requests
+import numpy
+import time
+import gensim
+import nltk
+
+import os
+
+
+
+class Word2Vec:
+    def __init__(self):
+        self.model = None
+        self.useNorm = False
+        self.model_path ='{}/data/word2vec_300.bin'.format(os.path.dirname(os.path.realpath(__file__)))
+        self.url = "http://www.robertoderesu.com/ml/word2vec/word-vec"
+        self.tokenizer = nltk.tokenize.RegexpTokenizer(r"\w{3,}")
+
+    def get_embeddings(self, texts):
+        if self.model is None:
+            self.load_model()
+
+        output = []
+        for text in texts:
+            words = self.tokenizer.tokenize(text)
+            words_embeddings = []
+
+            for word in words:
+                if word in self.model.vocab:
+                    word_embedding = self.model.word_vec(word, self.useNorm)
+                    words_embeddings.append([x.item() for x in word_embedding])
+
+            output.append(words_embeddings)
+
+        return output
+
+    def get_embeddings_remote(self, texts):
+        n_chunks = max(int(len(texts) / 100 + 1), 1)
+        # n_chunks = 1
+        chunks = [list(x) for x in numpy.array_split(numpy.array(texts), n_chunks)]
+
+        result = []
+        for text_chunk in chunks:
+            request_data = {
+                "texts": text_chunk,
+                "useNorm": False
+            }
+            response = requests.post(self.url, json=request_data)
+            response_data = response.json()
+            result.extend(response_data)
+
+        return result
+
+    def load_model(self):
+        print("Loading model...")
+        start_time_model = time.time()
+
+        self.model = gensim.models.KeyedVectors.load_word2vec_format(self.model_path, binary=True)
+
+        end_time_model = time.time()
+        print("Model loaded! Elapsed: " + str(end_time_model - start_time_model))
--- utils/__init__.py
+++ utils/__init__.py
@@ -1,2 +1,1 @@
 from utils import json_utils
-from utils import word2vec_utils
