2c7a37e3dae460bc170d515bdd368247d70a2d93
Roberto Deresu <roberto.deresu@gmail.com>
2019-06-10 10:35:54 +0300

    -

--- models/cae.py
+++ models/cae.py
@@ -117,7 +117,7 @@ class CAE:
         early_stopping = EarlyStopping(
             monitor='val_loss',
             min_delta=0.001,
-            patience=30,
+            patience=10,
             verbose=1,
             mode='min',
             restore_best_weights=True)
