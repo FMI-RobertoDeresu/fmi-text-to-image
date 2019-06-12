efa35f430d980378dfc818aca198ca2f20b066bd
Roberto Deresu <roberto.deresu@gmail.com>
2019-06-12 15:15:32 +0300

    tf imports

--- tf_imports.py
+++ tf_imports.py
@@ -1,10 +1,9 @@
 import tensorflow as tf
-from tensorflow.python.summary import summary as tf_summary
 from packaging import version
-from keras import backend as K
-from keras.utils import multi_gpu_model
 
 if version.parse(tf.__version__) > version.parse("1.10.0"):
+    # noinspection PyUnresolvedReferences, PyPep8Naming
+    from tensorflow.contrib.keras.api.keras import backend as K, optimizers, losses
     # noinspection PyUnresolvedReferences
     from tensorflow.contrib.keras.api.keras.layers import Input, Conv2D, MaxPooling2D, Conv2DTranspose
     # noinspection PyUnresolvedReferences
@@ -14,8 +13,12 @@ if version.parse(tf.__version__) > version.parse("1.10.0"):
     # noinspection PyUnresolvedReferences
     from tensorflow.contrib.keras.api.keras.callbacks import Callback, TensorBoard, ModelCheckpoint, EarlyStopping
     # noinspection PyUnresolvedReferences
-    from tensorflow.contrib.keras import optimizers, losses
+    from tensorflow.python.keras.utils import multi_gpu_model
+    # noinspection PyUnresolvedReferences
+    from tensorflow.python.summary import summary as tf_summary
 else:
+    # noinspection PyUnresolvedReferences
+    from tensorflow.keras import K, optimizers, losses
     # noinspection PyUnresolvedReferences
     from tensorflow.keras.layers import Input, Conv2D, MaxPooling2D, Conv2DTranspose
     # noinspection PyUnresolvedReferences
@@ -25,6 +28,8 @@ else:
     # noinspection PyUnresolvedReferences
     from tensorflow.keras.callbacks import Callback, TensorBoard, ModelCheckpoint
     # noinspection PyUnresolvedReferences
+    from tensorflow.python.keras.utils import multi_gpu_model
+    # noinspection PyUnresolvedReferences
     from keras.callbacks import EarlyStopping
     # noinspection PyUnresolvedReferences
-    from tensorflow.keras import optimizers, losses
+    from tensorflow.python.summary import summary as tf_summary
